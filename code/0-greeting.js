aedc34fbd9c4aa2a65259e62176dcbcaa52a1197
Kezza01 <mitcheke41@craighead.school.nz>
2023-05-23 10:54:33 +1200

    Added joke

--- code/0-greeting.js
+++ code/0-greeting.js
@@ -8,6 +8,12 @@ function greeting() {
   alert('Hello ' + username)
 }
 
+
+
 function joke() {
+alert ("I have a joke for you..")
+}
+let username = prompt(What has four wheels and flies) {
   // write the code for your joke here.
+alert("A rubbish truck!")
 }
